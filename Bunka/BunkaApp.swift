87d4f86398cb881d0185406d86c09717bd65200d
username <email@blahblah.com>
2023-06-26 14:00:10 +0200

    initial commit

---
+++ Bunka/BunkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BunkaApp.swift
+//  Bunka
+//
+//  Created by apprenant54 on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BunkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
